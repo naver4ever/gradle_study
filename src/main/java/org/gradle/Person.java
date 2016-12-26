280fdfd6f613b28d57320161ce2ea6fc10564daf
naver4ever <hak2gyo@gmail.com>
2016-12-26 15:09:36 +0900

    gradle_study create

---
+++ src/main/java/org/gradle/Person.java
@@ -0,0 +1,6 @@
+package org.gradle;
+
+
+public class Person {
+
+}
---
+++ src/test/java/org/gradle/PersonTest.java
@@ -0,0 +1,8 @@
+package org.gradle;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PersonTest {
+   
+}
