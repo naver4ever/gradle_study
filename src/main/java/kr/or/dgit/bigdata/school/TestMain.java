85dcc886eb2ab6eeccc9344bfd11762c33a8b4ae
naver4ever <hak2gyo@gmail.com>
2016-12-27 16:42:12 +0900

    master에서 고침

--- src/main/java/kr/or/dgit/bigdata/school/TestMain.java
+++ src/main/java/kr/or/dgit/bigdata/school/TestMain.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class TestMain {
 	public static void main(String[] args) {
 		JFrame jf = new JFrame();
-		jf.setTitle("테스트");
+		jf.setTitle("테스트2");
 		jf.setSize(300, 300);
 		jf.setVisible(true);
 	}
